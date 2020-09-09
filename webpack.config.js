9dac455ccc44c157743687e7dad6bd8519ff1a2c
Tatsiujin Chin <c910335@gmail.com>
2020-09-10 03:55:59 +0800

    initial

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+ 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '.'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '.'),
+    compress: true,
+    port: 5566
+  }
+}
